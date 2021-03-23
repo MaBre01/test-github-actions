230cee93deaf42fd26e4a881fd53c38d2ea47c5d
MaBre01 <mael.brevet@gmail.com>
2021-03-23 22:54:05 +0100

    Add symfony/http-foundation into the project

--- public/index.php
+++ public/index.php
@@ -1,3 +1,9 @@
 <?php
 
-echo 'Hello world!';
+use Symfony\Component\HttpFoundation\Request;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$request = Request::createFromGlobals();
+
+echo $request->getPathInfo();
